837fd7511d01ae25b73d7425fb2d49db4bee43d2
ArnoldNatte <arnold.natte@gmail.com>
2023-09-06 17:17:42 -0500

    aver este

--- projectMain.js
+++ projectMain.js
@@ -1,3 +1,4 @@
 console.log("this is a Project Main");
 console.log("why this archive not figure in the remote repository?");
 console.log("this edit you will se in the project-main archive");
+console.log("aver este");
